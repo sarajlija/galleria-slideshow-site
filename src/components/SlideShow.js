13c04a22208ad6a31b1283c9614627ae56976714
Salko <64722632+sarajlija@users.noreply.github.com>
2023-04-05 15:45:28 +0200

    up

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Carousel, Col, Row, Button, Modal, Card, Image } from "react-bootstrap"
+import { Carousel, Col, Row, Button, Modal, Card, Image } from "react-bootstrap"
 import dataslideshow from "../dataslideshow.json"
 
 import { useGlobalContext } from "../context"
@@ -8,15 +8,15 @@ function SlideShow() {
   const { show, handleClose, handleShow, selectedSlide, windowWidth, breakpointColumnsObj } = useGlobalContext()
 
   return (
-    <Carousel className="text-dark " interval={null}>
+    <Carousel className="text-dark mb-5" interval={null}>
       {dataslideshow.map(slide => (
         <Carousel.Item key={slide.images.gallery} className="">
           <Row>
-            <Col>
-              <Card className="border-0 ">
-                <Card.Img src={windowWidth > 576 ? slide.images.hero.large : slide.images.hero.small} alt={slide.name} />
+            <Col sm={12} md={6}>
+              <Card className="border-0" bsPrefix="card__image">
+                <Card.Img src={windowWidth > 768 ? slide.images.hero.large : slide.images.hero.small} alt={slide.name} />
                 <Card.Img src={slide.artist.image} alt="Card image" bsPrefix="img__artist" />
-                <Card.Body className=" text-dark" bsPrefix="card-body__slideshow">
+                <Card.Body className=" text-dark border-0" bsPrefix="card-body__slideshow">
                   <Card.Title>{slide.name}</Card.Title>
                 </Card.Body>
               </Card>
@@ -34,24 +34,30 @@ function SlideShow() {
                 </Modal>
               )}
             </Col>
-
-            <Col>
-              <Card className=" text-dark" bsPrefix="card__slide">
-                <Card.Text bsPrefix="card-text__slide opacity-100">{slide.description}</Card.Text>
+            <Col sm={12} md={6}>
+              <Card md={12} className=" text-dark border-0" bsPrefix="card__slide">
+                <Card.Text bsPrefix="card-text__slide opacity-100">
+                  {slide.description}
+                  <a className="source-link d-flex text-uppercase " href={slide.source}>
+                    go to source
+                  </a>
+                </Card.Text>
               </Card>{" "}
-              <Card bsPrefix="card__slide">
-                <Card.Text bsPrefix="card-text__year position-absolute">{slide.year}</Card.Text>
+              <Card bsPrefix="border-0">
+                <Card.Text bsPrefix="card-text__year">{slide.year}</Card.Text>
               </Card>
-            </Col>
+            </Col>{" "}
+            <Row>
+              <Col md={12}>
+                {" "}
+                <Card bsPrefix="card-galleria ">
+                  <hr className="w-50" />
+                  <Card.Title bsPrefix="card-title__galleria">{slide.name}</Card.Title>
+                  <Card.Text bsPrefix="card-text__galleria">{slide.artist.name}</Card.Text>
+                </Card>
+              </Col>
+            </Row>
           </Row>{" "}
-          <Row className="vh-40 d-flex align-items-center justify-content-start">
-            <Col>
-              <Card bsPrefix="card-galleria ">
-                <Card.Title bsPrefix="card-title__galleria">{slide.name}</Card.Title>
-                <Card.Text bsPrefix="card-text__galleria">{slide.artist.name}</Card.Text>
-              </Card>
-            </Col>
-          </Row>
         </Carousel.Item>
       ))}{" "}
     </Carousel>
